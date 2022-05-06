e40e8a29e3c4ca1dc7ce6b566b931e63cf1ce429
LucasTapa <lucas.dfa@live.com>
2022-05-06 10:40:48 -0300

    requisito 3 refeito

---
+++ desafio3.sql
@@ -0,0 +1,5 @@
+SELECT a.usuario AS 'usuario', COUNT(DISTINCT b.id_cancao) as 'qtde_musicas_ouvidas', ROUND(SUM(c.duracao_cancao) / 60, 2) total_minutos
+FROM SpotifyClone.users a join
+SpotifyClone.hst_reproducoes b on (a.usuario_id = b.usuario_id) join
+SpotifyClone.cancoes c on (b.id_cancao = c.id_cancao)
+GROUP BY a.usuario
